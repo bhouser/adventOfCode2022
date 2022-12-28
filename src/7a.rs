5cc7316d7cea5b1da5c846fe4647e7d133ba0535
Brian Houser <brian.houser@mappedin.ca>
2022-12-28 13:58:48 -0500

    wip

--- src/7a.rs
+++ src/7a.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{BufReader, BufRead}, collections::{HashMap, VecDeque},
+    io::{BufReader, BufRead, Lines}, collections::{HashMap, VecDeque}
 };
 
 fn main() {
@@ -9,12 +9,12 @@ fn main() {
     let file_path = "data/7.txt";
 
     let file = File::open(file_path).expect("file should be defined");
-    let lines = BufReader::new(file).lines();
+    let mut lines = BufReader::new(file).lines();
 
     #[derive(Debug)]
     struct FileItem {
         name: String,
-        size: i32,
+        size: u64,
     }
 
     #[derive(Debug)]
@@ -25,7 +25,7 @@ fn main() {
     }
 
     impl Dir {
-        fn size(&self) -> i32 {
+        fn size(&self) -> u64 {
             let mut size = 0;
             for (_, file) in self.child_files.iter() {
                 size += file.size;
@@ -39,111 +39,98 @@ fn main() {
         }
     }
 
-    let root_dir = Dir {
+    let mut root_dir = Dir {
         name: String::from("root"),
         child_dirs: HashMap::new(),
         child_files: HashMap::new(),
     };
 
-    let mut path_history = Vec::new();
+    fn add_small_dirs(current_dir: &mut Dir, mut total_small_dir_size: u64) -> u64 {
+        let current_dir_size = current_dir.size();
 
-    // TODO there will just be one pointer,
-    path_history.push(&root_dir);
+        for dir in current_dir.child_dirs.iter_mut() {
+            total_small_dir_size = add_small_dirs(dir.1, total_small_dir_size);
+        }
 
-
-
-
-    // TODO read this: https://www.reddit.com/r/rust/comments/cnjhup/idiomatic_way_to_reference_parent_struct/
-    // TODO maybe look at this: https://rust-unofficial.github.io/too-many-lists/
-
-//    let mut score = 0;
-    for line in lines {
-        if let Ok(l) = line {
-            let mut tokens: VecDeque<&str> = l.split(" ").collect();
-
-            println!("tokens: {:?}", tokens);
-
-            let first_token = tokens.pop_front().expect("first token should exist");
-
-            if first_token.eq("$") {
-                // it's a command
-                let command_token = tokens.pop_front().expect("command token should exist");
-                if command_token.eq("cd") {
-                    // it's a "cd"
-                    let dest_token = tokens.pop_front().expect("cd destination token should exist");
-
-                    // TODO ignore this command
-                    if dest_token.eq("/") {
-                        // do nothing
-                    } else if dest_token.eq("..") {
-                        // ah, this is where I need the back links
-                    } else {
-                        // move filesystem pointer to dir.  Dir must exist.
-
-                        println!("cd into this dir: {}", dest_token);
-                        let mut current_location: &mut Dir = *path_history.last().expect("path_history should not be empty");
-
-                        path_history.push(
-                                current_location.child_dirs.get_mut(dest_token).expect("dir should exist (have been discovered) before we cd into it")
-                        );
-
-//                        filesystem_pointer = filesystem_pointer.child_dirs.get_mut(dest_token).expect("dir should exist (have been discovered) before we cd into it");
-                    }
-                } // else: it's an "ls".  Do nothing.
-            } else if first_token.eq("dir") {
-                let new_dir_name_token = tokens.pop_front().expect("dir name token should exist");
-
-                let current_location = path_history.last().expect("path_history should not be empty");
-                let existing_dir_option = current_location.child_dirs.get(new_dir_name_token);
-                if let None = existing_dir_option {
-                    let new_dir = Dir {
-                        name: new_dir_name_token.into(),
-                        child_dirs: HashMap::new(),
-                        child_files: HashMap::new(),
-                    };
-
-                    current_location.child_dirs.insert(new_dir_name_token.into(), new_dir);
-                }
-            } else {
-                // it's a new file, so create one
-                let new_file_size = first_token.parse().expect("file size token should parse to a number");
-                let new_file_name = tokens.pop_front().expect("new file name token should exist");
-
-                let new_file = FileItem {
-                    name: new_file_name.into(),
-                    size: new_file_size
-                };
-                let current_location = path_history.last().expect("path_history should not be empty");
-                current_location.child_files.entry(new_file_name.into()).or_insert(new_file);
-            }
+        if current_dir_size <= 100000 {
+            total_small_dir_size + current_dir_size
         } else {
-            panic!("line was not Ok");
+            total_small_dir_size
         }
     }
 
-    println!("This is our filesystem tree {:?}", root_dir);
+    fn process_lines(lines: &mut Lines<BufReader<File>>, current_dir: &mut Dir) {
+        loop {
+            let line = lines.next();
+            if let Some(line) = line {
+                let line = line.expect("line should not error");
 
-//    println!("total score: {}", score);
-}
+                let mut tokens: VecDeque<&str> = line.split(" ").collect();
+                let first_token = tokens.pop_front().expect("first token should exist");
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+                if first_token.eq("$") {
+                    // it's a command
+                    let command_token = tokens.pop_front().expect("command token should exist");
+                    if command_token.eq("cd") {
+                        // it's a "cd"
+                        let dest_token = tokens.pop_front().expect("cd destination token should exist");
 
-    #[test]
-    fn it_works() {
-        let output = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        .chars()
-        .zip(1..53)
-        .collect::<HashMap<char, usize>>();
+                        if dest_token.eq("/") {
+                            // do nothing
+                            continue;
+                        } else if dest_token.eq("..") {
+                            return;
+                        } else {
+                            // dir must exist
+                            // println!("cd into this dir: {}", dest_token);
+                            let child_dir = current_dir.child_dirs.get_mut(dest_token)
+                                .expect("dir should exist (have been discovered) before we cd into it");
+                            process_lines(lines, child_dir);
+                        }
+                    } else {
+                        // it's an "ls" token
+                        continue;
+                    }
+                } else if first_token.eq("dir") {
+                    let new_dir_name_token = tokens.pop_front().expect("dir name token should exist");
 
-        println!("this is the output {:?}", output);
+                    // dir shouldn't exist unless we did "ls" twice in same dir
+                    let existing_dir_option = current_dir.child_dirs.get(new_dir_name_token);
+                    if let None = existing_dir_option {
+                        let new_dir = Dir {
+                            name: new_dir_name_token.into(),
+                            child_dirs: HashMap::new(),
+                            child_files: HashMap::new(),
+                        };
 
+                        current_dir.child_dirs.insert(new_dir_name_token.into(), new_dir);
+                    }
+                    continue;
+                } else {
+                    // it's a new file, so create one
+                    let new_file_size = first_token.parse().expect("file size token should parse to a number");
+                    let new_file_name = tokens.pop_front().expect("new file name token should exist");
 
-//        let a_string = "asdfgg";
-//
-//        let len = a_string.len() / 2;
-//
-//        assert_eq!(len, 4);
+                    let new_file = FileItem {
+                        name: new_file_name.into(),
+                        size: new_file_size
+                    };
+                    current_dir.child_files.entry(new_file_name.into()).or_insert(new_file);
+                    continue;
+                }
+            } else {
+                return;
+                // end of input, initiate return chain.
+            }
+        }
     }
+
+
+    process_lines(&mut lines, &mut root_dir);
+
+    // println!("the directory structure:\n{:?}", root_dir);
+
+    let result = add_small_dirs(&mut root_dir, 0);
+
+    println!("result {}", result);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,136 @@
 use std::{
     fs::File,
-    io::{BufReader, BufRead}, collections::HashSet,
+    io::{BufReader, BufRead, Lines}, collections::{HashMap, VecDeque}
 };
 
 fn main() {
     println!("Hello, worldddd!");
 
-    let file_path = "data/6.txt";
+    let file_path = "data/7.txt";
 
     let file = File::open(file_path).expect("file should be defined");
-    let line = BufReader::new(file).lines().next().expect("line should exist").expect("without error");
+    let mut lines = BufReader::new(file).lines();
 
-    let unique_char_sequence_len = 14;
-    for ch in line.char_indices() {
-        if ch.0 < unique_char_sequence_len {
-            continue;
-        }
-        let substring = &line[(ch.0 - unique_char_sequence_len)..ch.0];
-        let char_set: HashSet<char> = substring.chars().collect();
-        if char_set.len() == unique_char_sequence_len {
-            println!("index until unique 14: {}", ch.0);
-            break;
+    #[derive(Debug)]
+    struct FileItem {
+        name: String,
+        size: u64,
+    }
+
+    #[derive(Debug)]
+    struct Dir {
+        name: String,
+        child_dirs: HashMap<String, Dir>,
+        child_files: HashMap<String, FileItem>,
+    }
+
+    impl Dir {
+        fn size(&self) -> u64 {
+            let mut size = 0;
+            for (_, file) in self.child_files.iter() {
+                size += file.size;
+            }
+
+            for (_, dir) in self.child_dirs.iter() {
+                size += dir.size();
+            }
+
+            size
         }
     }
+
+    let mut root_dir = Dir {
+        name: String::from("root"),
+        child_dirs: HashMap::new(),
+        child_files: HashMap::new(),
+    };
+
+    fn add_small_dirs(current_dir: &mut Dir, mut total_small_dir_size: u64) -> u64 {
+        let current_dir_size = current_dir.size();
+
+        for dir in current_dir.child_dirs.iter_mut() {
+            total_small_dir_size = add_small_dirs(dir.1, total_small_dir_size);
+        }
+
+        if current_dir_size <= 100000 {
+            total_small_dir_size + current_dir_size
+        } else {
+            total_small_dir_size
+        }
+    }
+
+    fn process_lines(lines: &mut Lines<BufReader<File>>, current_dir: &mut Dir) {
+        loop {
+            let line = lines.next();
+            if let Some(line) = line {
+                let line = line.expect("line should not error");
+
+                let mut tokens: VecDeque<&str> = line.split(" ").collect();
+                let first_token = tokens.pop_front().expect("first token should exist");
+
+                if first_token.eq("$") {
+                    // it's a command
+                    let command_token = tokens.pop_front().expect("command token should exist");
+                    if command_token.eq("cd") {
+                        // it's a "cd"
+                        let dest_token = tokens.pop_front().expect("cd destination token should exist");
+
+                        if dest_token.eq("/") {
+                            // do nothing
+                            continue;
+                        } else if dest_token.eq("..") {
+                            return;
+                        } else {
+                            // dir must exist
+                            // println!("cd into this dir: {}", dest_token);
+                            let child_dir = current_dir.child_dirs.get_mut(dest_token)
+                                .expect("dir should exist (have been discovered) before we cd into it");
+                            process_lines(lines, child_dir);
+                        }
+                    } else {
+                        // it's an "ls" token
+                        continue;
+                    }
+                } else if first_token.eq("dir") {
+                    let new_dir_name_token = tokens.pop_front().expect("dir name token should exist");
+
+                    // dir shouldn't exist unless we did "ls" twice in same dir
+                    let existing_dir_option = current_dir.child_dirs.get(new_dir_name_token);
+                    if let None = existing_dir_option {
+                        let new_dir = Dir {
+                            name: new_dir_name_token.into(),
+                            child_dirs: HashMap::new(),
+                            child_files: HashMap::new(),
+                        };
+
+                        current_dir.child_dirs.insert(new_dir_name_token.into(), new_dir);
+                    }
+                    continue;
+                } else {
+                    // it's a new file, so create one
+                    let new_file_size = first_token.parse().expect("file size token should parse to a number");
+                    let new_file_name = tokens.pop_front().expect("new file name token should exist");
+
+                    let new_file = FileItem {
+                        name: new_file_name.into(),
+                        size: new_file_size
+                    };
+                    current_dir.child_files.entry(new_file_name.into()).or_insert(new_file);
+                    continue;
+                }
+            } else {
+                return;
+                // end of input, initiate return chain.
+            }
+        }
+    }
+
+
+    process_lines(&mut lines, &mut root_dir);
+
+    // println!("the directory structure:\n{:?}", root_dir);
+
+    let result = add_small_dirs(&mut root_dir, 0);
+
+    println!("result {}", result);
 }
